a4f58120abf2a3978b86eb667f1b570bceac9bf2
Raihan <raihannady@gmail.com>
2024-06-27 14:41:00 +0700

    create weather app

---
+++ app/index.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+// Import axios, BASE_URL, dan API_KEY
+import axios from "axios";
+import { BASE_URL, API_KEY } from "../constant";
+import { View, StyleSheet, ActivityIndicator } from "react-native";
+import WeatherSearch from "../components/weatherSearch";
+import WeatherInfo from "../components/weatherInfo";
+
+const App = () => {
+  // Definisikan state "weatherData" dan "setWeatherData"
+  const [weatherData, setWeatherData] = useState();
+  const [status, setStatus] = useState("");
+
+  const renderComponent = () => {
+    switch (status) {
+      case "loading":
+        return <ActivityIndicator size="large" />;
+      case "success":
+        return <WeatherInfo weatherData={weatherData} />;
+      case "error":
+        return (
+          <Text>
+            Something went wrong. Please try again with a correct city name.
+          </Text>
+        );
+      default:
+        return;
+    }
+  };
+
+  const searchWeather = (location) => {
+    // Mengatur status ke "loading"
+    setStatus("loading");
+    axios
+      .get(`${BASE_URL}?q=${location}&appid=${API_KEY}`)
+      .then((response) => {
+        const data = response.data;
+        data.visibility /= 1000;
+        data.visibility = data.visibility.toFixed(2);
+        data.main.temp -= 273.15;
+        data.main.temp = data.main.temp.toFixed(2);
+        setWeatherData(data);
+        // Mengatur status ke "success"
+        setStatus("success");
+      })
+      .catch((error) => {
+        // Mengatur status ke "error"
+        setStatus("error");
+      });
+  };
+
+  return (
+    <View style={styles.container}>
+      <WeatherSearch searchWeather={searchWeather} />
+      {/* Menggunakan function renderComponent di sini */}
+      <View style={styles.margintTop20}>{renderComponent()}</View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+});
+
+export default App;
---
+++ constant.js
@@ -0,0 +1,2 @@
+export const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+export const API_KEY = "5ce97733433f8679b62f1474c7e6d619";
